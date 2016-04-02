e892e494e315c4bae2b686c8a2cc12094e8a3e81
Iurii Lutsenko <lutsenko.yuriy@yandex.ru>
2016-04-02 15:17:23 +0300

    Предварительная версия главного экрана, наполнил фэйковой инфой

--- app/src/main/java/com/example/android/yamsd/ListOfArtistsActivityFragment.java
+++ app/src/main/java/com/example/android/yamsd/ListOfArtistsActivityFragment.java
@@ -1,22 +1,48 @@
 package com.example.android.yamsd;
 
-import android.support.v4.app.Fragment;
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 /**
  * A placeholder fragment containing a simple view.
  */
 public class ListOfArtistsActivityFragment extends Fragment {
 
+    ArrayAdapter<String> artistsListAdapter;
     public ListOfArtistsActivityFragment() {
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_list_of_artists, container, false);
+        View listOfArtists = inflater.inflate(R.layout.fragment_list_of_artists, container, false);
+
+        String[] artists = {
+                "Artist 1",
+                "Artist 2",
+                "Artist 3"
+        };
+        List<String> artistsList = new ArrayList<String>(Arrays.asList(artists));
+
+        artistsListAdapter =
+                new ArrayAdapter<String>(
+                        getActivity(),
+                        R.layout.single_artist,
+                        R.id.single_artist,
+                        artistsList
+                );
+        ListView listView = (ListView) listOfArtists.findViewById(R.id.artists_list);
+        listView.setAdapter(artistsListAdapter);
+
+        return listOfArtists;
     }
 }
